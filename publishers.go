826874a56712a2c93788b420ca64750d344c0fd2
Derek Smith <derek.smith@naveego.com>
2016-09-02 11:25:47 -0400

    Updated publishers.go

--- publishers.go
+++ publishers.go
@@ -1,13 +1,12 @@
 package publishers
 
-
 import (
-    "github.com/naveego/pipeline-api/publisher"
-    "github.com/naveego/pipeline-publishers/sql/mssql"
-    "github.com/naveego/pipeline-publishers/web/wellcast"
+	"github.com/naveego/pipeline-api/publisher"
+	"github.com/naveego/pipeline-publishers/sql/mssql"
+	"github.com/naveego/pipeline-publishers/web/wellcast"
 )
 
-func init(){
-    publisher.RegisterFactory("mssql", func() publisher.Factory { return mssql.Publisher{} })
-    publisher.RegisterFactory("wellcast", func() publisher.Publisher { return wellcast.Publisher{} )
+func init() {
+	publisher.RegisterFactory("mssql", func() publisher.Factory { return mssql.Publisher{} })
+	publisher.RegisterFactory("wellcast", func() publisher.Publisher { return wellcast.Publisher{} })
 }
