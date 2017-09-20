a7363ce2c610aff9cdd75aaebeaac0505ec1d81d
AzureAD\SteveRuble <steve.ruble@naveego.com>
2017-09-20 15:50:15 -0400

    PIPE-18 Renamed mssql to mssql-pub (new convention for pub/sub names).

--- sql/mssql-pub/main.go
+++ sql/mssql-pub/main.go
@@ -1,9 +1,8 @@
 // Copyright © 2017 Naveego
 
-
 package main
 
-import "github.com/naveego/pipeline-publishers/sql/mssql/cmd"
+import "github.com/naveego/pipeline-publishers/sql/mssql-pub/cmd"
 
 func main() {
 	cmd.Execute()
